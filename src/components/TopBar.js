2f7fa1a6b3f29d63a40aafefac1ce2e90bac831e
Engin Yenice <enginyenice2626@gmail.com>
2021-01-20 02:52:11 +0300

    Text Edit

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -19,7 +19,7 @@ export default function TopBar() {
       border: "none",
       
     }}>
-        <Card.Title className="text-center">Kocali Üniversitesi İstatistik</Card.Title>
+        <Card.Title className="text-center">Kocaeli Üniversitesi İstatistik</Card.Title>
         <Card.Subtitle className="mb-2 text-muted text-center">3. Parti Uygulama</Card.Subtitle>
     </Card>
     </>
